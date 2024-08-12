f692aaae6d648fdf6b047561125c4f9cadbdef6b
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-08-12 16:17:17 -0300

    Refinando a busca por filmes

--- screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,12 +5,21 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para buscar: ");
+        var busca = leitura.nextLine();
+        System.out.println(busca);
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=81c2fe9f";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=81c2fe9f"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
